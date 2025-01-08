761011a8c2f5cdbe3d565d86e4c7dbda0e93d5bb
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-01-08 23:49:47 +0530

    Add tuples

--- src/bin/tuples.rs
+++ src/bin/tuples.rs
@@ -7,7 +7,18 @@ fn main(){
     // destructuring the tuple
     let(a, b, c) = t;
 
-    println!("Print the value of a: {a}");
-    println!("Print the value of b: {b}");
-    println!("Print the value of b: {c}");
+    println!("Print the value of a: {}", t.0);
+    println!("Print the value of b: {}", t.1);
+    println!("Print the value of b: {}", t.2);
+
+    // ignore with _
+
+    let (_, b, _) = t;
+
+    // empty tuple
+
+    let t = ();
+
+    // nested tuple
+    let nested = ((1.23, 'a'), (true, 1u32, 'b'), ());
 }
