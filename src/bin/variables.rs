f57432dc5d4ac7f411a54f452f83345805a67e87
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-01-08 23:34:21 +0530

    Add variables

---
+++ src/bin/variables.rs
@@ -0,0 +1,20 @@
+#![allow(unused)]
+
+fn main(){
+    let x = 12;
+    // x += 1; This will throw an error, Will not compile.
+    println!("value of x is: {x}");
+
+    let mut y = 20;
+    y += 2;
+    println!("value of y is: {y}");
+
+    const x : i32 = 5; // For constant you have to define the type
+
+    let y = true;
+
+    // let v : Vec<{integer}> = vec![1,2,3];
+
+    // println!("Value of this vector: {v}");
+
+}
