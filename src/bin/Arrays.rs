1bff85c08bb85f3024dff6e0df6b696c93443e40
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-01-09 23:38:04 +0530

    Add Arrays

---
+++ src/bin/Arrays.rs
@@ -0,0 +1,10 @@
+#![allow(unused)]
+
+fn main(){
+    //Arrays
+    let arr: [u32; 3] = [1,2,3];
+    println!("Array: {:?}", arr);
+
+    let arr: [u32; 10] = [0; 10];
+    println!("Initialise array with value 0: {:?}", arr)
+}
