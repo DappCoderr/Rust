9a99d9dcdb9fe6eb6e2459af85018355977d792d
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-03-13 12:34:42 +0530

    function

--- src/bin/functions.rs
+++ src/bin/functions.rs
@@ -16,10 +16,10 @@ fn get_letter_from_sentence(sentence: String, num:usize) -> char {
 fn get_first_word_from_sentence(sentence:String) -> String {
     let mut ans = String::from("");
     for chars in sentence.chars(){
-        ans.push_str(chars.to_string().as_str());
         if chars == ' '{
             break;
         }
+        ans.push_str(chars.to_string().as_str());
     }
     return ans;
 }
